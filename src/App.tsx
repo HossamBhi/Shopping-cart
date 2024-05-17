ac9e20669929eeedc49f3c86a52fcc6e4ae2e08c
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2024-05-17 18:23:14 +0300

    Init project with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="font-bold text-center text-9xl">Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
