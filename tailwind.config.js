38ff1d2f9744f9865e420cae9836d82356edee47
Hossam Bhi Pasha <hossambasha9900@gmail.com>
2024-05-20 22:52:49 +0300

    Add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        background: "var(--color-background)",
+        "background-card": "var(--color-background-card)",
+      },
+    },
   },
   plugins: [],
 }
